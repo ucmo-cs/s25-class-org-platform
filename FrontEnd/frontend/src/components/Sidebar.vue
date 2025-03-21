8e8c5c6bd03cbd5489fab27b7a7e4d5fca9afc09
1justus <1justuswilliams@gmail.com>
2025-03-21 14:16:21 -0500

    Adding Vue Resources and Initial Pages.

---
+++ FrontEnd/frontend/src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<script>
+  export default {
+
+  }
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+  .sidebar {
+    margin: 0;
+    width: 350px;
+    height: 100%;
+    text-align: left;
+    background: #2c3e50;
+    display: inline-block;
+  }
+</style>
