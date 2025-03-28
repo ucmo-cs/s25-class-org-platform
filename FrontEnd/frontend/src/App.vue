7cb49285086029c6c7560e2da4133bfaf76fa91d
1justus <1justuswilliams@gmail.com>
2025-03-28 13:06:12 -0500

    Updated the menu bars and calendar files.

--- FrontEnd/frontend/src/App.vue
+++ FrontEnd/frontend/src/App.vue
@@ -1,37 +1,65 @@
 <script>
   import Calendar from './components/Calendar.vue'
-  
+  import Class from "./components/Class.vue";
 
   export default {
     components: {
-      Calendar
+      Calendar,
+      Class
+    },
+    data() {
+      return {
+        currentPage: "Calendar",
+        semester: "Spring 2025"
+      }
+    },
+    methods: {
+      navigateToHome() {
+        this.currentPage = "Calendar"
+      },
+      navigateToClass(className) {
+        this.currentPage = "Class"
+      }
     }
   }
 </script>
 
-
-
-
 <template>
-  <head>
-    <title></title>
-  </head>
   <body>
-    <div class="sideBar"></div>
-    <div class="topBar"></div>
-    <Calendar/>
-
+    <div class="topBar">
+      <button @click="navigateToHome">Student Helper</button>
+    </div>
+    <div class="sideBar">
+      <h1>{{ semester }}</h1>
+      <hr>
+      <button @click="navigateToClass(className)">Class</button>
+      <hr>
+      <button @click="navigateToClass(className)">Class</button>
+      <hr>
+      <button @click="navigateToClass(className)">Class</button>
+      <hr>
+    </div>
+    <div class="mainScreen">
+      <Calendar v-if="currentPage === 'Calendar'" />
+      <Class v-if="currentPage === 'Class'" />
+    </div>
   </body>
 </template>
 
-  
 <style>
-html, body {
+  html, body {
+      width: 100%;
+      height: 100%;
+      margin: 0;
+      padding: 0;
+      display: flex;
+  }
+  .mainScreen {
+    margin-top: 100px;
+    margin-left: 300px;
     width: 100%;
     height: 100%;
-    margin: 0;
-    padding: 0;
-    display: flex;
+    position: fixed;
   }
   .topBar {
     width: 100%;
@@ -40,12 +68,34 @@ html, body {
     position: fixed;
     margin-top: 0;
   }
+  .topBar button {
+    background-color: #222222;
+    width: 300px;
+    height: 100%;
+    color: white;
+    font-size: 25px;
+    cursor: pointer;
+    border: none;
+  }
   .sideBar {
     margin-top: 100px;
     margin-left: 0;
-    width: 350px;
+    width: 300px;
     height: 100%;
     background: #2c3e50;
     position: fixed;
+  }
+  .sideBar h1 {
+    text-align: center;
+    color: white;
+  }
+  .sideBar button {
+    background-color: #2c3e50;
+    width: 300px;
+    height: 50px;
+    color: white;
+    font-size: 15px;
+    cursor: pointer;
+    border: none;
   }
 </style>
--- FrontEnd/frontend/src/components/Calendar.vue
+++ FrontEnd/frontend/src/components/Calendar.vue
@@ -1,67 +1,222 @@
 <template>
- <!--Calendar-->
+  <!--Calendar-->
   <div class="calendar">
-    <h2>January 2025</h2>
+    <h2>
+      <button @click="prevMonth">Previous Month</button>
+      {{ currentMonth }} {{ currentYear }}
+      <button @click="nextMonth">Next Month</button>
+    </h2>
     <table>
       <thead>
-        <tr>
-          <th v-for="day in daysOfWeek" :key="day">{{ day }}</th>
-        </tr>
+      <tr>
+        <th v-for="day in daysOfWeek" :key="day">{{ day }}</th>
+      </tr>
       </thead>
       <tbody>
-        <tr v-for="(week, index) in weeks" :key="index">
-          <td v-for="(day, i) in week" :key="i">
-            {{ day || '' }}
-          </td>
-        </tr>
+      <tr v-for="(week, index) in weeks" :key="index">
+        <td v-for="(day, i) in week" :key="i">
+          {{ day || '' }}
+        </td>
+      </tr>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-export default {
-data() {
-  return {
-    daysOfWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    weeks: [
-      [null, null, null, null, null, 1, 2],
-      [3, 4, 5, 6, 7, 8, 9],
-      [10, 11, 12, 13, 14, 15, 16],
-      [17, 18, 19, 20, 21, 22, 23],
-      [24, 25, 26, 27, 28, 29, 30],
-      [31, null, null, null, null, null]
-    ]
-  };
-}
-};
+  export default {
+    data() {
+      return {
+        monthNum: new Date().getMonth() + 1,
+        currentMonth: this.returnMonth(new Date().getMonth() + 1),
+        currentYear: new Date().getFullYear(),
+        daysOfWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        weeks: [
+          [null, null, null, null, null, null, 1],
+          [2, 3, 4, 5, 6, 7, 8],
+          [9, 10, 11, 12, 13, 14, 15],
+          [16, 17, 18, 19, 20, 21, 22],
+          [23, 24, 25, 26, 27, 28, 29],
+          [30, 31, null, null, null, null]
+        ]
+      };
+    },
+    methods: {
+      prevMonth() {
+        if (this.monthNum === 1) {
+          this.monthNum = 12;
+          this.currentYear--;
+        } else {
+          this.monthNum--;
+        }
+        this.currentMonth = this.returnMonth(this.monthNum);
+      },
+      nextMonth() {
+        if (this.monthNum === 12) {
+          this.monthNum = 1;
+          this.currentYear++;
+        } else {
+          this.monthNum++;
+        }
+        this.currentMonth = this.returnMonth(this.monthNum);
+      },
+      returnMonth(month) {
+        switch (month) {
+          case(1):
+            this.weeks = [
+              [null, null, 1, 2, 3, 4, 5],
+              [6, 7, 8, 9, 10, 11, 12],
+              [13, 14, 15, 16, 17, 18, 19],
+              [20, 21, 22, 23, 24, 25, 26],
+              [27, 28, 29, 30, 31, null, null],
+              [null, null, null, null, null, null]
+            ]
+            return "January";
+          case(2):
+            this.weeks = [
+              [null, null, null, null, null, null, 1],
+              [2, 3, 4, 5, 6, 7, 8],
+              [9, 10, 11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20, 21, 22],
+              [23, 24, 25, 26, 27, 28, null],
+              [null, null, null, null, null, null]
+            ]
+            return "February";
+          case(3):
+            this.weeks = [
+              [null, null, null, null, null, null, 1],
+              [2, 3, 4, 5, 6, 7, 8],
+              [9, 10, 11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20, 21, 22],
+              [23, 24, 25, 26, 27, 28, 29],
+              [30, 31, null, null, null, null]
+            ]
+            return "March";
+          case(4):
+            this.weeks = [
+              [null, null, 1, 2, 3, 4, 5],
+              [6, 7, 8, 9, 10, 11, 12],
+              [13, 14, 15, 16, 17, 18, 19],
+              [20, 21, 22, 23, 24, 25, 26],
+              [27, 28, 29, 30, null, null, null],
+              [null, null, null, null, null, null]
+            ]
+            return "April";
+          case(5):
+            this.weeks = [
+              [null, null, null, null, 1, 2, 3],
+              [4, 5, 6, 7, 8, 9, 10],
+              [11, 12, 13, 14, 15, 16, 17],
+              [18, 19, 20, 21, 22, 23, 24],
+              [25, 26, 27, 28, 29, 30, 31],
+              [null, null, null, null, null, null]
+            ]
+            return "May";
+          case(6):
+            this.weeks = [
+              [1, 2, 3, 4, 5, 6, 7],
+              [8, 9, 10, 11, 12, 13, 14],
+              [15, 16, 17, 18, 19, 20, 21],
+              [22, 23, 24, 25, 26, 27, 28],
+              [29, 30, null, null, null, null, null],
+              [null, null, null, null, null, null]
+            ]
+            return "June";
+          case(7):
+            this.weeks = [
+              [null, null, 1, 2, 3, 4, 5],
+              [6, 7, 8, 9, 10, 11, 12],
+              [13, 14, 15, 16, 17, 18, 19],
+              [20, 21, 22, 23, 24, 25, 26],
+              [27, 28, 29, 30, 31, null, null],
+              [null, null, null, null, null, null]
+            ]
+            return "July";
+          case(8):
+            this.weeks = [
+              [null, null, null, null, null, 1, 2],
+              [3, 4, 5, 6, 7, 8, 9],
+              [10, 11, 12, 13, 14, 15, 16],
+              [17, 18, 19, 20, 21, 22, 23],
+              [24, 25, 26, 27, 28, 29, 30],
+              [31, null, null, null, null, null]
+            ]
+            return "August";
+          case(9):
+            this.weeks = [
+              [null, 1, 2, 3, 4, 5, 6],
+              [7, 8, 9, 10, 11, 12, 13],
+              [14, 15, 16, 17, 18, 19, 20],
+              [21, 22, 23, 24, 25, 26, 27],
+              [28, 29, 30, null, null, null, null],
+              [null, null, null, null, null, null]
+            ]
+            return "September";
+          case(10):
+            this.weeks = [
+              [null, null, null, 1, 2, 3, 4],
+              [5, 6, 7, 8, 9, 10, 11],
+              [12, 13, 14, 15, 16, 17, 18],
+              [19, 20, 21, 22, 23, 24, 25],
+              [26, 27, 28, 29, 30, 31, null],
+              [null, null, null, null, null, null]
+            ]
+            return "October";
+          case(11):
+            this.weeks = [
+              [null, null, null, null, null, null, 1],
+              [2, 3, 4, 5, 6, 7, 8],
+              [9, 10, 11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20, 21, 22],
+              [23, 24, 25, 26, 27, 28, 29],
+              [30, null, null, null, null, null]
+            ]
+            return "November";
+          case(12):
+            this.weeks = [
+              [null, 1, 2, 3, 4, 5, 6],
+              [7, 8, 9, 10, 11, 12, 13],
+              [14, 15, 16, 17, 18, 19, 20],
+              [21, 22, 23, 24, 25, 26, 27],
+              [28, 29, 30, 31, null, null, null],
+              [null, null, null, null, null, null]
+            ]
+            return "December";
+        }
+      }
+    }
+  }
 </script>
 
-<style>
-table {
-border-spacing: 15px;
-text-align: center;
-border: 1px solid black;
-padding-bottom: 10px;
-}
-td {
-padding: 10px;
-background-color: white;
-text-align: center;
-padding-bottom: 50px;
-padding-right: 150px;
-}
-th {
-padding: 10px;
-background-color: white;
-text-align: center;
-}
-h2 {
-text-align: center;
-padding-top: 100px;
-}
-.calendar {
-  margin-top: 50px;
-  margin-left: 450px;
-}
+<style scoped>
+    table {
+      border: 2px solid #2c3e50;
+      border-collapse: collapse;
+      height: 100%;
+      width: 100%;
+    }
+    th {
+      text-align: center;
+      background-color: white;
+      padding: 25px;
+    }
+    td {
+      padding-top: 10px;
+      padding-left: 10px;
+      padding-bottom: 100px;
+      padding-right: 175px;
+      text-align: left;
+      background-color: white;
+      border: 1px solid #2c3e50;
+    }
+    h2 {
+      text-align: center;
+    }
+    .calendar {
+      display: inline-block;
+      align-items: center;
+      justify-content: center;
+      margin-left: 5%;
+      margin-right: 5%;
+    }
 </style>
