21b84742abef417177f19aceb775cda4fc8a7b94
MilesL35 <96485567+MilesL35@users.noreply.github.com>
2025-04-21 12:34:56 -0500

    Updated the database dump file because I forgot to do that earlier

--- DataBase/Dump20250403.sql
+++ DataBase/Dump20250403.sql
@@ -33,6 +33,7 @@ CREATE TABLE `class` (
   `UserID` int NOT NULL,
   `Instructor` varchar(255) DEFAULT NULL,
   `Description` varchar(255) DEFAULT NULL,
+  `OfficeLocation` varchar(255) DEFAULT NULL,
   `OfficeHoursID` int DEFAULT NULL,
   `InstructorPhone` varchar(30) DEFAULT NULL,
   `InstructorEmail` varchar(255) DEFAULT NULL,
@@ -57,7 +58,7 @@ CREATE TABLE `class` (
 
 LOCK TABLES `class` WRITE;
 /*!40000 ALTER TABLE `class` DISABLE KEYS */;
-INSERT INTO `class` VALUES (1,'Algorithm Desighn',1,1,'WCM 105',1,'Yousef','Learn to deisgn and analyze algorithms',1,'816-555-1234','Yousef@ucmo.edu','Algorithm Stuff','2025-01-12','2025-05-10'),(2,'Operating Systems',1,5,'WCM 231',1,'Park','Learn about threads and stuff',6,'816-555-1234','Park@ucmo.edu','','2025-01-12','2025-05-10'),(3,'Senior Project',1,7,'WCM 131',1,'Grebe and Johnson','Make a big project and go way overboard on the scope',8,'816-555-1234','Johnson@ucmo.edu','','2025-01-12','2025-05-10'),(5,'Compputer Networking',1,13,'Online',1,'Jin','Learn about computer networking',14,'816-555-1234','jin@ucmo.edu','Computer Networking stuff','2025-01-12','2025-05-10');
+INSERT INTO `class` VALUES (1,'Algorithm Desighn',1,1,'WCM 105',1,'Yousef','Learn to deisgn and analyze algorithms','WCM 207',1,'816-555-1234','Yousef@ucmo.edu','Algorithm Stuff','2025-01-12','2025-05-10'),(2,'Operating Systems',1,5,'WCM 231',1,'Park','Learn about threads and stuff','WCM ???',6,'816-555-1234','Park@ucmo.edu','','2025-01-12','2025-05-10'),(3,'Senior Project',1,7,'WCM 131',1,'Grebe and Johnson','Make a big project and go way overboard on the scope','WCM 12?',8,'816-555-1234','Johnson@ucmo.edu','','2025-01-12','2025-05-10'),(5,'Compputer Networking',1,13,'Online',1,'Jin','Learn about computer networking','Zoom',14,'816-555-1234','jin@ucmo.edu','Computer Networking stuff','2025-01-12','2025-05-10');
 /*!40000 ALTER TABLE `class` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,11 +77,15 @@ CREATE TABLE `event` (
   `End` datetime DEFAULT NULL,
   `ClassID` int DEFAULT NULL,
   `UserID` int NOT NULL,
+  `isHomework` bool default null,
+  `file` int default null,
   PRIMARY KEY (`EventID`),
   KEY `UserID` (`UserID`),
   KEY `event_ibfk_1_idx` (`ClassID`),
+  KEY `event_ibfk_2_idx` (`file`),
   CONSTRAINT `event_ibfk_1` FOREIGN KEY (`ClassID`) REFERENCES `class` (`ClassID`),
-  CONSTRAINT `event_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`)
+  CONSTRAINT `event_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  CONSTRAINT `event_ibfk_3` FOREIGN KEY (`file`) REFERENCES `files` (`FileID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -90,9 +95,17 @@ CREATE TABLE `event` (
 
 LOCK TABLES `event` WRITE;
 /*!40000 ALTER TABLE `event` DISABLE KEYS */;
-INSERT INTO `event` VALUES (1,'HW1','HW1 is due.','2025-01-21 00:00:00','2025-01-21 00:00:00',1,1),(2,'Host Smash tournament','Host a big smash tournement in the untion building','2025-03-29 08:00:00','2025-03-29 22:00:00',NULL,2),(3,'HW2','HW2 is due.','2025-02-21 00:00:00','2025-02-21 00:00:00',5,1),(4,'HW3','HW3 is due.','2025-03-21 00:00:00','2025-03-21 00:00:00',5,1),(6,'Random','Random','2025-03-21 00:00:00','2025-03-21 00:00:00',NULL,1);
+INSERT INTO `event` VALUES (1,'HW1','HW1 is due.','2025-01-21 00:00:00','2025-01-21 00:00:00',1,1, TRUE, null),(2,'Host Smash tournament','Host a big smash tournement in the untion building','2025-03-29 08:00:00','2025-03-29 22:00:00',NULL,2, FALSE, NULL),(3,'HW2','HW2 is due.','2025-02-21 00:00:00','2025-02-21 00:00:00',5,1, TRUE, NULL),(4,'HW3','HW3 is due.','2025-03-21 00:00:00','2025-03-21 00:00:00',5,1,TRUE, NULL),(6,'Random','Random','2025-03-21 00:00:00','2025-03-21 00:00:00',NULL,1, FALSE, NULL);
 /*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
+
+DROP TABLE IF exists `files`;
+
+CREATE TABLE `files` (
+	`FileID` INT NOT NULL auto_increment,
+    `Data` mediumblob DEFAULT NULL,
+    primary key (`FileID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Table structure for table `meetingtimes`
@@ -145,7 +158,9 @@ CREATE TABLE `notes` (
   `Notes` int DEFAULT NULL,
   PRIMARY KEY (`NotesID`),
   KEY `notes_ifbk_1_idx` (`ClassID`),
-  CONSTRAINT `notes_ifbk_1` FOREIGN KEY (`ClassID`) REFERENCES `class` (`ClassID`)
+  KEY `notes_ifbk_2_idx` (`Notes`),
+  CONSTRAINT `notes_ifbk_1` FOREIGN KEY (`ClassID`) REFERENCES `class` (`ClassID`),
+  CONSTRAINT `notes_ifbk_2` FOREIGN KEY (`Notes`) REFERENCES `files` (`FileID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -155,7 +170,7 @@ CREATE TABLE `notes` (
 
 LOCK TABLES `notes` WRITE;
 /*!40000 ALTER TABLE `notes` DISABLE KEYS */;
-INSERT INTO `notes` VALUES (1,1,'2025-01-01',100),(2,1,'2025-01-01',200),(3,2,'2025-01-01',700),(4,2,'2025-01-01',200);
+INSERT INTO `notes` VALUES (1,1,'2025-01-01',NULL),(2,1,'2025-01-01',NULL),(3,2,'2025-01-01',NULL),(4,2,'2025-01-01',NULL);
 /*!40000 ALTER TABLE `notes` ENABLE KEYS */;
 UNLOCK TABLES;
 
